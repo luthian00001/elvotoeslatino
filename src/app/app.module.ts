acfeb30575bbb55ba02fcb8ae24b95e83a403a39
Daniel Phelps <daniel@exnihlo.com>
2018-07-03 10:16:38 -0500

    think I got the layout workingish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router"
+import { FlexLayoutModule } from "@angular/flex-layout"
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { routes } from "./app.routing"
+import { MaterialModule } from "./material/material.module"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,6 @@
+import { Route } from "@angular/router"
+import { IndexComponent } from "./index/index.component"
+
+export var routes: Route[] = [
+    { path: "", component: IndexComponent }
+]
