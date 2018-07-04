e21cf06ac663dacb5881f1667e193185c8b2eb71
Daniel Phelps <daniel@exnihlo.com>
2018-07-04 11:59:11 -0500

    got nav working and some other stuff

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from "@angular/material"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule } from "@angular/material"
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports:[
-    MatToolbarModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: []
 })
